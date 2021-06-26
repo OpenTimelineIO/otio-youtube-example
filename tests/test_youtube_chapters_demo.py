2df3ed0d7b02efeb0e270ce5c2f120bad92766a7
Utsab Saha <utsab.k.saha@gmail.com>
2021-06-26 14:59:13 -0700

    Setup code for tests for downloading the youtube video and description

---
+++ tests/test_youtube_chapters_demo.py
@@ -0,0 +1,29 @@
+
+
+
+import os
+import sys
+import unittest
+
+
+
+youtube_url = "https://www.youtube.com/watch?v=RBSGKlAvoiM"
+video_file = "video.mp4"
+
+
+# run the youtube_chapters_demo example...
+
+
+# Tests
+
+# Test 1: Verify that an mp4 file is created disk. 
+# os.path.isfile(video_file)
+
+
+# Test 2: Verify that a txt file is created on disk.
+
+
+
+
+
+

0e96e737259595ed90344b35dac43cfb9637f278
Utsab Saha <utsab.k.saha@gmail.com>
2021-08-07 14:43:44 -0700

    Refactor the example to import youtube-dl in python instead of invoking youtube-dl via commandline

--- src/youtube_chapters_demo.py
+++ src/youtube_chapters_demo.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import youtube_dl
+
 import opentimelineio as otio
 
 
@@ -11,4 +13,44 @@ videoFileName = youtubeURL + ".mp4"
 
 print("youtube url: ", youtubeURL)
 
-os.system("youtube-dl --restrict-filenames --write-description -o '%s' %s" % (videoFileName, youtubeURL))
+#os.system("youtube-dl --restrict-filenames --write-description -o '%s' %s" % (videoFileName, youtubeURL))
+
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'tmp/%(id)s.mp4',
+    'noplaylist': True,
+    'quiet': True,
+    'forceduration':True
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    dictMeta = ydl.extract_info(
+        "https://www.youtube.com/watch?v={sID}".format(sID=youtubeURL),
+        download=True)
+
+
+
+
+
+
+
+# Steps for importing the video into OTIO
+# 1. Create a TimeLine object
+# timeline = otio.schema.TimeLine()
+# timeline.name = "Youtube Demo"
+
+# # 2. Create a Track on the timeline 
+# track = otio.schema.Track()
+# track.name = "Videos"
+# timeline.tracks.append(track)
+
+# 3. Find out how long the youtube video is
+#available_range = otio.opentime.TimeRange(?,?)
+
+
+# 4. Create a media_referene (contians the file path of the youtube video)
+# 5. Create a Clip and set the media_reference (based on what we found in step 4) 
+# 6. Append the Clip to the Track
+

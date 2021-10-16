959ef79b1c74912d3d0d6f351515ff2456e2ad6e
Utsab Saha <utsab.k.saha@gmail.com>
2021-10-16 14:29:46 -0700

    Refactor code to use more helper functions

--- src/youtube_chapters_demo.py
+++ src/youtube_chapters_demo.py
@@ -77,90 +77,101 @@ def create_markers(chapters, fps):
 
 
 
-youtubeURL = sys.argv[1]
+def download_from_youtube(youtubeURL): 
+    ydl_opts = {
+        'outtmpl': 'tmp/%(id)s.mp4',
+        'noplaylist': True,
+        'quiet': True,
+        'writedescription' : True
+    }
 
-videoFileName = youtubeURL + ".mp4"
-
-
-
-ydl_opts = {
-    'outtmpl': 'tmp/%(id)s.mp4',
-    'noplaylist': True,
-    'quiet': True,
-    'writedescription' : True
-}
-
-# Download the youtube video and description onto local computer 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    dictMeta = ydl.extract_info(
-        "https://www.youtube.com/watch?v={sID}".format(sID=youtubeURL),
-        download=True)
+    # Download the youtube video and description onto local computer 
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        dictMeta = ydl.extract_info(
+            "https://www.youtube.com/watch?v={sID}".format(sID=youtubeURL),
+            download=True)
+    
+    return dictMeta
     
 
 
+def create_timeline(dictMeta, video_file_name): 
+    # Steps for importing the video into OTIO
+    # 1. Create a TimeLine object
+    timeline = otio.schema.Timeline()
+    timeline.name = "Youtube Demo"
+
+    # 2. Create a Track on the timeline 
+    track = otio.schema.Track()
+    track.name = "Videos"
+    timeline.tracks.append(track)
+
+    # 3. Find out how long the youtube video is
+    totalFrames = dictMeta['duration'] * dictMeta['fps']
 
 
-# Steps for importing the video into OTIO
-# 1. Create a TimeLine object
-timeline = otio.schema.Timeline()
-timeline.name = "Youtube Demo"
-
-# 2. Create a Track on the timeline 
-track = otio.schema.Track()
-track.name = "Videos"
-timeline.tracks.append(track)
-
-# 3. Find out how long the youtube video is
-totalFrames = dictMeta['duration'] * dictMeta['fps']
-
-
-available_range = otio.opentime.TimeRange(
-    otio.opentime.RationalTime(0, dictMeta['fps']),
-    otio.opentime.RationalTime(totalFrames, dictMeta['fps'])
-)
-
-
-# 4. Create a media_reference (contians the file path of the youtube video)
-
-media_reference = otio.schema.ExternalReference(
-        target_url="tmp/" + videoFileName,
-        available_range=available_range
+    available_range = otio.opentime.TimeRange(
+        otio.opentime.RationalTime(0, dictMeta['fps']),
+        otio.opentime.RationalTime(totalFrames, dictMeta['fps'])
     )
 
-# 5. Create a Clip and set the media_reference (based on what we found in step 4) 
-clip = otio.schema.Clip(name="Youtube clip")
-clip.media_reference = media_reference
+
+    # 4. Create a media_reference (contians the file path of the youtube video)
+
+    media_reference = otio.schema.ExternalReference(
+            target_url="tmp/" + video_file_name,
+            available_range=available_range
+        )
+
+    # 5. Create a Clip and set the media_reference (based on what we found in step 4) 
+    clip = otio.schema.Clip(name="Youtube clip")
+    clip.media_reference = media_reference
 
 
-# 6. Append the Clip to the Track
-track.append(clip)
-
-
-
-# 7. Process the youtube description and insert Markers into the timeline
-description_file = "tmp/" + youtubeURL + ".description"
-chapters = process_youtube_description(description_file) 
-
-markers = create_markers(chapters, dictMeta['fps'])
-
-
-for marker in markers:
-    clip.markers.append(marker)
+    # 6. Append the Clip to the Track
+    track.append(clip)
 
 
 
+    # 7. Process the youtube description and insert Markers into the timeline
+    description_file = "tmp/" + youtubeURL + ".description"
+    chapters = process_youtube_description(description_file) 
+
+    markers = create_markers(chapters, dictMeta['fps'])
 
 
-#save the timeline as .otio file 
+    for marker in markers:
+        clip.markers.append(marker)
 
-otio_filename = youtubeURL + ".otio"
-otio.adapters.write_to_file(timeline, otio_filename)
-print(
-    "SAVED: {0} with {1} clips.".format(
-        otio_filename,
-        len(timeline.tracks[0])
+
+
+
+
+    #save the timeline as .otio file 
+
+    otio_filename = youtubeURL + ".otio"
+    otio.adapters.write_to_file(timeline, otio_filename)
+    print(
+        "SAVED: {0} with {1} clips.".format(
+            otio_filename,
+            len(timeline.tracks[0])
+        )
     )
-)
+
+
+
+youtubeURL = sys.argv[1]
+video_file_name = youtubeURL + ".mp4"
+
+dictMeta = download_from_youtube(youtubeURL)
+
+create_timeline(dictMeta, video_file_name)
+
+
+
+
+
+
 
 
 
